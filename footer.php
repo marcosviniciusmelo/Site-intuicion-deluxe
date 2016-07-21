866592914c74fdd31c9aeeb3811a60cef5983b1d
Marcos Vinicius Melo <marcos234melo@gmail.com>
2016-07-21 00:58:46 -0300

    V 0.1
    Front da home

---
+++ footer.php
@@ -0,0 +1,6 @@
+	<footer>
+		<div class="main">© COPYRIGHT INTUICION DELUXE</div>
+	</footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Intuicion Deluxe</title>
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<header>
+		<div class="bar-top">
+			<div class="main">
+				<a href="">ESPAÇO DA REVENDEDORA</a>
+			</div>
+		</div>
+
+		<div class="content">
+			<div class="main">
+				<div class="logo"><a href=""><img src="img/logo.png" alt=""></a></div>
+				<nav>
+					<ul class="nav-menu">
+						<li><a href="">A EMPRESA</a></li>
+						<li><a href="">NOSSAS LOJAS</a></li>
+						<li><a href="">CAMPANHA</a></li>
+						<li><a href="">FILME</a></li>
+						<li><a href="">CLIPPING</a></li>
+						<li><a href="">SEJA REVENDEDOR(A)</a></li>
+						<li><a href="">LOJA ONLINE</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
